d341617f09631015ba91da28115c9a750d9a7ba5
mariawu2016 <pistilwu@qq.com>
2018-02-22 23:39:07 +0800

    更新邮件发送功能

--- pipelines.py
+++ pipelines.py
@@ -13,6 +13,12 @@ import datetime
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
+#发送邮件
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+#有编码问题
 class BookscrapyPipeline(object):
     def __init__(self):
         now_time = datetime.datetime.now()
@@ -29,6 +35,7 @@ class BookscrapyPipeline(object):
         self.file.write(line)
         return item
 
+#无编码问题
 class BookscrapyPipelineToExcel(object):
     def __init__(self):
         now_time = datetime.datetime.now()
@@ -43,7 +50,20 @@ class BookscrapyPipelineToExcel(object):
         self.ws.append(line)
         #self.wb.save('./'+self.wbname)
         #下载文件、封皮、词云并生成邮件发送
-        
+        #send list
+        durls=item['downloadurl'].split('|')
+        ds=""
+        for i in range(0,3):
+            durls[i]=u"<a href='"+durls[i]+u"' style='display:block;'>下载"+str(i+1)+u"</a>"
+            ds=ds+durls[i]
+
+        mailto_list = ["pistilwu@qq.com",]
+        mail_title = item['bookname']+"("+item['author']+')['+item['bookclass']+']['+item['topclass']+u'][书包网小说]'
+        mail_content = u"<table width=600><tr><td width=120>书名</td><td width=480>"+item['bookname']+u"</td></tr><tr><td>简介</td><td>"+item['introduction']+u"</td></tr><tr><td>下载链接</td><td>"+ds+"</td></tr></table>"
+        #mail_content="<table><tr><td>简介</td><td>"+item['introduction']+u"</td></tr><tr><td>下载链接</td><td>"+durls.join()+"</td></tr></table>"
+        mm = Mailer(mailto_list,mail_title,mail_content)
+        res = mm.sendMail()
+        print res
         return item
         
     def close_spider(self,spider):
@@ -81,3 +101,68 @@ class MySQLPipeline(object):
 
     def handle_error(self, e):
         log.err(e)
+
+#邮件发送类
+class Mailer(object):
+    def __init__(self,maillist,mailtitle,mailcontent):#,mailattpath,mailattname):
+        self.mail_list = maillist
+        self.mail_title = mailtitle
+        self.mail_content = mailcontent
+
+        self.mail_host = "smtp.qq.com"
+        self.mail_user = "pistilwu"
+        self.mail_pass = "pebvsnwogxlrcacj"
+        self.mail_postfix = "qq.com"
+
+    def sendMail(self):
+ 
+        me = self.mail_user + "<" + self.mail_user + "@" + self.mail_postfix + ">"
+        msg = MIMEMultipart()
+        msg['Subject'] =self.mail_title
+        msg['From'] = me
+        msg['To'] = ";".join(self.mail_list)
+
+        puretext = MIMEText('<div>'+self.mail_content+'</div>','html','utf-8')
+        #puretext = MIMEText('纯文本内容'+self.mail_content)
+        msg.attach(puretext)
+
+        # 构造附件
+        #att = MIMEText(open(mailattpath+mailattname, "rb").read(), "base64", "utf-8")
+        #att["Content-Type"] = "application/octet-stream"
+        # 附件名称为中文时的写法
+        #att.add_header("Content-Disposition", "attachment", filename=("gbk", "", mailattname))
+        # 附件名称非中文时的写法
+        #att["Content-Disposition"] = 'attachment; filename="test.txt"'
+        #print(1)
+        #msg.attach(att)
+
+        # jpg类型的附件
+        #jpgpart = MIMEApplication(open('/home/mypan/1949777163775279642.jpg', 'rb').read())
+        #jpgpart.add_header('Content-Disposition', 'attachment', filename='beauty.jpg')
+        #msg.attach(jpgpart)
+
+        # 首先是xlsx类型的附件
+        #xlsxpart = MIMEApplication(open('test.xlsx', 'rb').read())
+        #xlsxpart.add_header('Content-Disposition', 'attachment', filename='test.xlsx')
+        #msg.attach(xlsxpart)
+
+        # mp3类型的附件
+        #mp3part = MIMEApplication(open('kenny.mp3', 'rb').read())
+        #mp3part.add_header('Content-Disposition', 'attachment', filename='benny.mp3')
+        #msg.attach(mp3part)
+
+        # pdf类型附件
+        #part = MIMEApplication(open('foo.pdf', 'rb').read())
+        #part.add_header('Content-Disposition', 'attachment', filename="foo.pdf")
+        #msg.attach(part)
+
+        try:
+            s = smtplib.SMTP() #创建邮件服务器对象
+            s.connect(self.mail_host) #连接到指定的smtp服务器。参数分别表示smpt主机和端口
+            s.login(self.mail_user+"@"+self.mail_postfix, self.mail_pass) #登录到你邮箱
+            s.sendmail(me, self.mail_list, msg.as_string()) #发送内容
+            s.close()
+            return True
+        except Exception, e:
+            print str(e)
+            return False
